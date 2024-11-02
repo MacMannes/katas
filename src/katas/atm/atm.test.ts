caa6979a786afcba95cd9cd76632dbad36089779
André Mathlener <andre.mathlener@ns.nl>
2024-11-02 15:52:09 +0100

    Test boundaries

--- src/katas/atm/atm.test.ts
+++ src/katas/atm/atm.test.ts
@@ -1,7 +1,16 @@
 import { describe, expect, it } from 'vitest';
+import { ATM } from '@katas/atm/atm';
+
+/**
+ * Nominal values 10, 20, 50, 100, 200 and 500 dollars.
+ */
 
 describe('ATM Kata', () => {
-    it('should work', () => {
-        expect(true).toBeTruthy();
+    const atm = new ATM();
+
+    it('should return -1 when amount is out of range', () => {
+        expect(atm.withdraw(9)).toBe(-1);
+        expect(atm.withdraw(-1)).toBe(-1);
+        expect(atm.withdraw(501)).toBe(-1);
     });
 });
---
+++ src/katas/atm/atm.ts
@@ -0,0 +1,5 @@
+export class ATM {
+    withdraw(number: number): number {
+        return -1;
+    }
+}
