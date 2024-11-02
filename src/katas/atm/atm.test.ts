32dbcab07959ededbb3a520e390e76f0f429ca90
André Mathlener <andre.mathlener@ns.nl>
2024-11-02 15:58:09 +0100

    Make sure invalid banknotes return -1

--- src/katas/atm/atm.test.ts
+++ src/katas/atm/atm.test.ts
@@ -11,7 +11,7 @@ describe('ATM Kata', () => {
     it('should return -1 when amount is out of range', () => {
         expect(atm.withdraw(9)).toBe(-1);
         expect(atm.withdraw(-1)).toBe(-1);
-        expect(atm.withdraw(501)).toBe(-1);
+        expect(atm.withdraw(1501)).toBe(-1);
     });
 
     it('should return 1 for when one of the nominal values are withdrawn', () => {
@@ -19,7 +19,13 @@ describe('ATM Kata', () => {
         expect(atm.withdraw(20)).toBe(1);
         expect(atm.withdraw(50)).toBe(1);
         expect(atm.withdraw(100)).toBe(1);
-        expect(atm.withdraw(20)).toBe(1);
+        expect(atm.withdraw(200)).toBe(1);
         expect(atm.withdraw(500)).toBe(1);
+    });
+
+    it('should return -1 when amount is within boundaries, but is not a valid note', () => {
+        expect(atm.withdraw(5)).toBe(-1);
+        expect(atm.withdraw(25)).toBe(-1);
+        expect(atm.withdraw(250)).toBe(-1);
     });
 });
--- src/katas/atm/atm.ts
+++ src/katas/atm/atm.ts
@@ -1,8 +1,12 @@
+const BANK_NOTES = [10, 20, 50, 100, 200, 500];
+
 export class ATM {
     withdraw(number: number): number {
         if (number < 10 || number > 500) {
             return -1;
         }
+
+        if (!BANK_NOTES.includes(number)) return -1;
 
         return 1;
     }
