a90fdea56e0872553669a600da1a2dd417ac28d8
André Mathlener <andre.mathlener@ns.nl>
2024-11-03 14:53:25 +0100

    Also check for room description

--- src/katas/katacombs/domain/game.test.ts
+++ src/katas/katacombs/domain/game.test.ts
@@ -5,9 +5,11 @@ describe('Game', () => {
     const game = new Game();
 
     describe('start', () => {
-        it('should print the title of the first room', () => {
-            const result = game.start();
+        it('should print the title and description of the first room', () => {
+            const result = game.start().toUpperCase();
             expect(result).toContain('LOST IN SHOREDITCH');
+            expect(result).toContain('YOU ARE STANDING AT THE END OF A BRICK LANE');
+            expect(result).toContain('DOWN A GULLY.');
         });
     });
 });
--- src/katas/katacombs/domain/game.ts
+++ src/katas/katacombs/domain/game.ts
@@ -1,5 +1,8 @@
 export class Game {
     public start(): string {
-        return 'LOST IN SHOREDITCH\n\n';
+        return (
+            'Lost in Shoreditch\n\nYou are standing at the end of a brick lane before a small brick building called the old truman brewery.\n' +
+            'Around you is a forest of restaurants and bars. A small stream of crafted beer flows out of the building and down a gully.'
+        );
     }
 }
