1d77486f4af8be5c592e95efa95b2af508dc3bf9
André Mathlener <andre.mathlener@ns.nl>
2024-11-03 16:50:04 +0100

    Extract method groupRooms

--- src/katas/katacombs/domain/game.ts
+++ src/katas/katacombs/domain/game.ts
@@ -3,12 +3,18 @@ import { UserInterface } from '@katas/katacombs/domain/ui';
 import { groupBy } from '@utils/array';
 
 export class Game {
-    private readonly rooms: Record<string, Room> = {};
+    private readonly roomsByName: Record<string, Room> = {};
 
     constructor(
         private readonly ui: UserInterface,
         rooms: Room[],
     ) {
+        this.roomsByName = this.groupRooms(rooms);
+    }
+
+    private groupRooms(rooms: Room[]): Record<string, Room> {
+        const result: Record<string, Room> = {};
+
         const groupedRooms = groupBy(rooms, (room) => room.name);
         for (const name in groupedRooms) {
             const roomToAdd = groupedRooms[name];
@@ -16,11 +22,13 @@ export class Game {
                 throw new Error('Two different rooms with the same name are not allowed');
             }
 
-            this.rooms[name] = roomToAdd[0];
+            result[name] = roomToAdd[0];
         }
+
+        return result;
     }
 
     public start(): void {
-        this.ui.displayRoom(this.rooms['start']);
+        this.ui.displayRoom(this.roomsByName['start']);
     }
 }
